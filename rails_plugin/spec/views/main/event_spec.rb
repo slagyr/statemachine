3f18e52c0442e9e470c8523827b47b9ce7a10156
slagyr <slagyr@47b43dd7-c81f-0410-affe-a56df1242653>
2006-11-29 04:35:15 +0000

    fixed broken specs

--- rails_plugin/spec/views/main/event_spec.rb
+++ rails_plugin/spec/views/main/event_spec.rb
@@ -12,13 +12,20 @@ context "Event View" do
 
   setup do
     @display = mock("display")
-    assigns[:context] = @display
     @display.stub!(:message).and_return("$123.45")
     @display.stub!(:affordable_items).and_return([Stub.new(1)])
     @display.stub!(:non_affordable_items).and_return([Stub.new(2)])
     @display.stub!(:sold_out_items).and_return([Stub.new(3)])
     @display.stub!(:dispensed_item).and_return(Product.new(:name => "Milk"))
     @display.stub!(:change).and_return("$0.25")
+    
+    @vending_machine = mock("vending machine")
+    @vending_machine.stub!(:location).and_return("Downstairs")
+    @vending_machine.stub!(:cash_str).and_return("$10.00")
+    @vending_machine.stub!(:products).and_return([])
+    
+    assigns[:context] = @display
+    assigns[:vending_machine] = @vending_machine
     
     render "/main/event"
   end
@@ -28,11 +35,11 @@ context "Event View" do
   end
   
   specify "affordable items" do
-    response.body.should_include "document.getElementById('product_1').class = 'affordable'"
+    response.body.should_include "document.getElementById('product_1').className = 'affordable'"
   end
 
   specify "non affordable items" do
-    response.body.should_include "document.getElementById('product_2').class = 'non_affordable'"
+    response.body.should_include "document.getElementById('product_2').className = 'non_affordable'"
   end
 
   specify "sold_out" do
