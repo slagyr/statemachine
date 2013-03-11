98ec09aa8413e15220825ba9a122de9debb4f40b
Will Sobel <will@systeminsights.com>
2013-03-11 10:57:06 -0700

    More advanced dot graph generator. Handles substates, default transitions, and hierarchal transitions to superstates as cluster boxes.

--- lib/statemachine/generate/dot_graph/dot_graph_statemachine.rb
+++ lib/statemachine/generate/dot_graph/dot_graph_statemachine.rb
@@ -39,15 +39,30 @@ module Statemachine
 
         def explore_sm
           @nodes = []
-          @transitions = []
-          @sm.states.values.each { |state|
-            state.transitions.values.each { |transition|
-              @nodes << transition.origin_id
-              @nodes << transition.destination_id
-              @transitions << transition
-            }
-          }
-          @nodes = @nodes.uniq
+           @transitions = []
+           @sm.states.values.each { |state|
+             state.transitions.values.each { |transition|
+               @nodes << transition.origin_id
+               if transition.destination_id.to_s =~ /_H$/
+                 dest = transition.destination_id.to_s.sub(/_H$/, '').to_sym
+                 @nodes << dest
+                 @transitions << Transition.new(transition.origin_id, dest, transition.event.to_s + '_H', nil)
+               else
+                 @nodes << transition.destination_id
+                 @transitions << transition
+               end
+             }
+             if Superstate === state and state.startstate_id
+               @nodes << state.startstate_id
+               @transitions << Transition.new(state.id, state.startstate_id, :start, nil)
+             end
+             if state.default_transition
+               transition = state.default_transition
+               @transitions << Transition.new(transition.origin_id, transition.destination_id, '*', nil)
+             end
+           }
+           @transitions = @transitions.uniq
+           @nodes = @nodes.uniq
         end
 
         def build_full_graph
@@ -55,7 +70,29 @@ module Statemachine
 
           add_graph_header(builder, "main")
 
-          @nodes.each { |node| add_node(builder, node) }
+          # Create graph tree
+          @sm.states.values.each { |state|
+            class << state
+              attr_reader :children
+              def add_child(child)
+                (@children ||= []) << child
+              end
+            end
+          }
+
+          roots = []
+          @sm.states.values.each { |state|
+            if state.superstate.id == :root
+              roots << state
+            else
+              state.superstate.add_child(state)
+            end
+          }
+
+          roots.each do |root|
+            add_node(builder, root)
+          end
+
           builder << endl
 
           @transitions.each do |transition|
@@ -83,6 +120,8 @@ module Statemachine
 
         def add_graph_header(builder, graph_name)
           builder << "digraph #{graph_name} {" << endl
+          builder << "  compound=true; compress=true; rankdir=LR;"
+          builder << endl
           builder.indent!
         end
 
@@ -92,9 +131,24 @@ module Statemachine
         end
 
         def add_node(builder, node)
-          builder << node
-          builder << " [ href = \"#{node}.svg\"]"
-          builder << endl
+          if Superstate === node
+            builder << "subgraph cluster_#{node.id} { "
+            builder.indent!
+            builder << endl
+            builder << "label = \"#{node.id}\"; style=rounded; #{node.id} "
+            builder << " [ style=\"rounded,filled\", shape=box, href=\"#{node.id}.svg\" ];"
+            builder << endl
+            node.children.each do |child|
+              add_node(builder, child)
+            end
+            builder.undent!
+            builder << "}"
+            builder << endl
+          else
+            builder << node.id
+            builder << " [shape=box, style=rounded, href=\"#{node.id}.svg\" ]"
+            builder << endl
+          end
         end
 
         def add_transition(builder, transition)
@@ -102,7 +156,11 @@ module Statemachine
           builder << " -> "
           builder << transition.destination_id
           builder << " [ "
-          builder << "label = #{transition.event} "
+          builder << "label = \"#{transition.event}\" "
+          if transition.event.to_s =~ /_H$/
+            dest = 'cluster_' + transition.destination_id.to_s
+            builder << ", lhead=#{dest}"
+          end
           builder << "]"
           builder << endl
         end
